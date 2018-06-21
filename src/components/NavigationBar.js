c7da1b888ae45b894fad98c03fef2f6837503863
boren <baybora.oren@gmail.com>
2018-06-21 15:42:50 +1000

    refactor/change navbar logo to title

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -16,7 +16,7 @@ class NavigationBar extends Component {
 
 
         return (
-            <Navbar brand='logo' right>
+            <Navbar brand='Would You Rather' right>
                 <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
                 <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
                 <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
